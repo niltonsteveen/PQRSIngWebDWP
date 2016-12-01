834719ab21b896af1e6af0267681b9b4d6d8b992
nilto <nilto@DESKTOP-2JJF6FV>
2016-11-30 18:01:42 -1000

    YA se ve bonito al menos todo lo demás, pero no logré crear la vista de listar PQR y también falla lo de las imagenes.

--- WebContent/bootstrapTemplate/js/login.js
+++ WebContent/bootstrapTemplate/js/login.js
@@ -1,19 +1,24 @@
 /**
  * Modulo de AngularJS que nos permite contar con toda la funcionalidad al lado del
- * cliente para la gestión de los clientes en el sistema
- * @autor Elver Suárez Alzate - elver.suarez@udea.edu.co
+ * cliente para la gestiÃ³n de los clientes en el sistema
+ * @autor Elver SuÃ¡rez Alzate - elver.suarez@udea.edu.co
  */
 
 var appCliente = angular.module('PQRS', ['ngRoute']);
 
+var username='';
+
 /**
- * Define los servicios necesario para la autenticación del usuario
+ * Define los servicios necesario para la autenticaciÃ³n del usuario
  */
+
+
+
 appCliente.service('Usuario', function($http){
 	
 	/**
 	 * Llama el servicio web para autenticar.
-	 */
+	 */	 	
 	this.autenticar = function(usuario, pws){
 		return $http({			
 			method: 'GET',
@@ -27,42 +32,62 @@ appCliente.service('Usuario', function($http){
 });
 
 /**
- * Configura el route de la aplicación para trabajar con Single Page Application
+ * Configura el route de la aplicaciÃ³n para trabajar con Single Page Application
  */
 appCliente.config(['$routeProvider', function($routeProvider){
 	
 	$routeProvider.when('/',{
-		templateUrl: 'pages/loginModal.html',
+		templateUrl: '/WebContent/bootstrapTemplate/index.html',
 		controller: 'contLogin'
-	});
-	
-	$routeProvider.when('/principal',{
-		templateUrl: 'index.html',
-		controller: 'contLoged'
 	});
 	
 }]);
 
+
+appCliente.provider('datoU',function(){
+	return{
+		$get: function(){
+			
+			return{
+				
+				getUsername: function(){
+					return username;
+				},
+				setUsername:function(valor){
+					username=valor;
+				}
+			}
+		}
+	}
+})
+
+
+appCliente.controller('contLoged', function($scope, $location, Usuario, datoU){
+	$scope.usuario=datoU.getUsername();
+	alert(datoU.getUsername());
+});
+
+
 /**
- * Controlador para funcionalidad de la autenticación del usuario
+ * Controlador para funcionalidad de la autenticaciÃ³n del usuario
  */
-appCliente.controller('contLogin', function($scope, $location, Usuario){
+appCliente.controller('contLogin', function($scope, $location, Usuario, datoU){
 	
 	$scope.nombreUsuario="";
 	$scope.contrasena="";	
-	
+	$scope.username="";
 	$scope.autenticar = function(){
 		Usuario.autenticar($scope.nombreUsuario,$scope.contrasena).success(function(data){
+			$scope.username=datoU.setUsername(data.correo);
 			if(data != ''){
 				$scope.nombreUsuario="";
 				$scope.contrasena="";
+				
 				$('#modalLogin').modal('toggle');
 				$('#liRegistrarse').remove();
 				$('#liInicioSesion').remove();
-				
-				$('#mostrar1').text('nilton'); /* aca va el data.username que no se como ponerlo. 
-				*/
-				alert("autenticacion correcta");
+				$('#header1').text(data.username);
+				alert(datoU.getUsername());
 				$location.url("/principal");
 				return;
 			}else{
